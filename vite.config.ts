61539fff85c947acd8f8d7ed8a99712ee8d95616
maopengjie <113440530+maopengjie@users.noreply.github.com>
2025-05-21 15:23:03 +0800

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
-/*
- * @Author: maopengjie 113440530+maopengjie@users.noreply.github.com
- * @Date: 2025-05-21 14:34:28
- * @LastEditors: maopengjie 113440530+maopengjie@users.noreply.github.com
- * @LastEditTime: 2025-05-21 14:43:19
- * @FilePath: /feed-wall-app/vite.config.ts
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import { fileURLToPath, URL } from 'node:url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default defineConfig({
   plugins: [vue()],
